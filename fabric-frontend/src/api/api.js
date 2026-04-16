8a9d729602226d658e0b02271d9dbbc8bcdd8507
Sitanshu <sitanshumathukia@gmail.com>
2026-04-16 13:15:44 +0530

    fixing frontend

--- fabric-frontend/src/api/api.js
+++ fabric-frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://fabric-backend-td8i.onrender.com",
 });
 
 export const createBatch = (data) => API.post("/batches", data);
