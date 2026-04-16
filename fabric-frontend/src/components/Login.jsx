42b95bc04c5150623dea76d4db9e5c74254db7af
Sitanshu <sitanshumathukia@gmail.com>
2026-04-17 01:15:18 +0530

    pushing

--- fabric-frontend/src/components/Login.jsx
+++ fabric-frontend/src/components/Login.jsx
@@ -49,12 +49,6 @@ export default function Login() {
           Login
         </button>
 
-        <p className="mt-4 text-center">
-          Don’t have an account?{" "}
-          <Link to="/register" className="text-blue-600 hover:underline">
-            Register here
-          </Link>
-        </p>
       </div>
     </div>
   );
